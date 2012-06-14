8159e95fc5050499a03d67adf55fbe9609f03627
msabourin <mathieu.c.sabourin@gmail.com>
2012-06-13 21:46:14 -0700

    Bumped version number.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 setup(
     name = "python-saucerest",
     packages = ['saucelabs'],
-    version = "0.0.2",
+    version = "0.0.3",
     author = "Mathieu Sabourin",
     author_email = "mathieu.c.sabourin@gmail.com",
     maintainer = "Mathieu Sabourin",
